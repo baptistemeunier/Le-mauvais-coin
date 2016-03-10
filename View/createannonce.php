bcdad192591391c048f30e1917235968a32e7bad
baptiste_meunier <baptistemeunier0@gmail.com>
2016-03-10 02:49:57 +0100

    Ajout de l'ajout d'annonce
    (no validation, no css, no add cat, no add user, ....)

--- View/createannonce.php
+++ View/createannonce.php
@@ -1,5 +1,13 @@
 <?php include 'header.php'; // Appel du template contenant les balises <head>, <header> ?>
 
-
+<?= $form->create("#"); ?>
+<?= $form->input('titre'); ?><br />
+<?= $form->textarea('description'); ?><br />
+<?= $form->input('email'); ?> <?= $form->input('tel'); ?><br />
+<?= $form->select('categorie', $categories); ?> <?= $form->input('categorie_autre'); ?><br />
+<?= $form->select('ville', $villes); ?>  <?= $form->input('ville_autre'); ?><br />
+<?= $form->input('prix', array("type" => "number")); ?><br />
+<?= $form->input('submit', array("type" => "submit", "value" => "Publier votre annonce !")); ?>
+<?= $form->close(); ?>
 <?php include 'footer.php'; // Appel du template contenant les balises <footer> ?>
 
--- create.php
+++ create.php
@@ -7,7 +7,11 @@
 /* Chargement de autoloader et des classe nessesaire */
 require_once "Class/autoloader.php";
 $App = new App();
+if(!empty($_POST)){
+	$App->getDBInstance()->AddAnnonce($_POST);
+}
+$categories = $App->getDBInstance()->findAllCategories();
+$villes     = $App->getDBInstance()->findAllVilles();
 
-
-echo $App->getTemplate()->render("createannonce");
+echo $App->getTemplate()->render("createannonce", array('form' => new Form($_POST), 'categories' => $categories, 'villes' => $villes));
 
