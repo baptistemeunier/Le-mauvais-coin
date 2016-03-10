a66630cf8dcdce3774d0371b8a9813cc791141ea
baptiste_meunier <baptistemeunier0@gmail.com>
2016-03-10 03:44:38 +0100

    Ajout du style form

--- View/header.php
+++ View/header.php
@@ -6,30 +6,31 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title><?= (isset($title))?$title:'Le Mauvais Coin - Site de petite annonces'?></title>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="css/base.css">
-</head>
-<body>
-<div id="page">
-	<header>
-		<div>
-		</div>
-		<nav>
-			<ul>
-				<li><a href="index.php">Accueil</a></li>
-				<li><a href="#">Recherche</a>
+	<head>
+		<title><?= (isset($title))?$title:'Le Mauvais Coin - Site de petite annonces'?></title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/base.css">
+		<link rel="stylesheet" type="text/css" href="css/form.css">
+	</head>
+	<body>
+		<div id="page">
+			<header>
+				<div>
+				</div>
+				<nav>
 					<ul>
-						<li><a href="categorie.php">Par catégorie</a></li>
-						<li><a href="ville.php">Par ville</a></li>
-						<li><a href="recherche.php">Recherche avancée</a></li>
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="#">Recherche</a>
+							<ul>
+								<li><a href="categorie.php">Par catégorie</a></li>
+								<li><a href="ville.php">Par ville</a></li>
+								<li><a href="recherche.php">Recherche avancée</a></li>
+							</ul>
+						</li>
+						<li style="float:right"><a href="create.php">Ajoutez votre annonce !</a></li>
+
 					</ul>
-				</li>
-				<li style="float:right"><a href="create.php">Ajoutez votre annonce !</a></li>
+				</nav>
+			</header>
 
-			</ul>
-		</nav>
-	</header>
-
-	<h1><?= (isset($titre))?$titre:'Bienvenue sur le Mauvais Coin'?></h1>
+			<h1><?= (isset($titre))?$titre:'Bienvenue sur le Mauvais Coin'?></h1>
