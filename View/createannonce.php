cc46eda452aff64e0868f8be682c8b7074995cb7
baptiste_meunier <baptistemeunier0@gmail.com>
2016-03-10 03:51:20 +0100

    Fix list view et indente createview

--- View/createannonce.php
+++ View/createannonce.php
@@ -6,16 +6,24 @@
 		<?= $form->create("#"); ?>
 		<label for="titre">Titre de l'annonce : </label>
 		<?= $form->input('titre', array('class' => 'inline')); ?><br />
-		<label for="titre">Description de l'annonce : </label>
+
+		<label for="description">Description de l'annonce : </label>
 		<?= $form->textarea('description'); ?><br />
-		<label for="titre">Vos coordonées : </label>
-		<?= $form->input('email'); ?> <?= $form->input('tel'); ?><br />
-		<label for="titre">Catégorie de l'annonce : </label>
-		<?= $form->select('categorie', $categories); ?> <?= $form->input('categorie_autre'); ?><br />
-		<label for="titre">Choisiser votre ville : </label>
+
+		<label for="email">Vos coordonées : </label>
+		<?= $form->input('email'); ?>
+		<?= $form->input('tel'); ?><br />
+
+		<label for="categorie">Catégorie de l'annonce : </label>
+		<?= $form->select('categorie', $categories); ?>
+		<?= $form->input('categorie_autre'); ?><br />
+
+		<label for="ville">Choisiser votre ville : </label>
 		<?= $form->select('ville', $villes); ?>  <?= $form->input('ville_autre'); ?><br />
-		<label for="titre">Definiser votre prix (Peut être nul) : </label>
+
+		<label for="prix">Definiser votre prix (Peut être nul) : </label>
 		<?= $form->input('prix', array("type" => "number")); ?><br />
+
 		<?= $form->input('submit', array("type" => "submit", "value" => "Publier votre annonce !")); ?>
 		<?= $form->close(); ?>
 	</div>
