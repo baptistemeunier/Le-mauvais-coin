6a0c51e6d64da1b747dcd7135ca442a1ec7fe040
baptiste_meunier <baptistemeunier0@gmail.com>
2016-03-25 09:16:25 +0100

    Fix Regions Repo

--- Class/App.php
+++ Class/App.php
@@ -104,8 +104,6 @@ class App
 		if($user === false){
 			$this->session->setMessage("<b>Connexion imposible !</b> Cette email ne corespond à aucun compte");
 		}else{
-			dump($user->getMdp());
-			dump(hash('sha256', $mdp));
 			if($user->getMdp()==hash('sha256', $mdp)){
 				$this->session->setMessage("<b>Connexion reussi !</b> Vous étes maintenant connecté", 'valid');
 				$this->session->set("_user", serialize($user));
