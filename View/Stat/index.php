8f2ba503ac923d59a783cb3122698e6a656164d5
baptiste_meunier <baptistemeunier0@gmail.com>
2016-04-27 15:52:39 +0200

    Ajout de graph

--- View/Stat/index.php
+++ View/Stat/index.php
@@ -1,10 +1,8 @@
 <?php include ROOT.'/View/header.php'; // Appel du template contenant les balises <head>, <header> ?>
-<div class="grille">
-	<div class="collone collone-6">
-			Depuis sa création le site enregistre <strong><?= $annonces ?> annonces.</strong><br />
-			A ce jour il y a <strong><?= $users ?> actifs enregistrés.</strong><br />
-			La categorie la plus utilisée est la catégorie <strong><?= $categorie['nom'] ?></strong> avec <?= $categorie['count'] ?> annonces.<br />
-			La ville la plus active est la ville de <strong><?= $ville['nom'] ?></strong> avec <?= $ville['count'] ?> annonces.
-	</div>
-</div>
+	Depuis sa création le site enregistre <strong><?= $annonces ?> annonces.</strong><br />
+	A ce jour il y a <strong><?= $users ?> actifs enregistrés.</strong><br />
+<?php
+	Graph::graphColonne($categories, "Graphique du nombres d'annnonce par categories");
+	Graph::graphColonne($ville, "Graphique du nombres d'annnonce par ville");
+?>
 <?php include ROOT.'/View/footer.php'; // Appel du template contenant les balises <footer> ?>
