021de0e94c520fa18788648c34c43654171ca7bc
baptiste_meunier <baptistemeunier0@gmail.com>
2016-04-27 16:12:48 +0200

    Fix graph

--- Core/Graph.php
+++ Core/Graph.php
@@ -23,7 +23,6 @@ class Graph
 	});
 	/* Intervalle entre chaque collonne et chaque hauteur de collonne */
 	$translate_y = 400/max($array);
-<<<<<<< HEAD
 	$translate_x = 1000/count($array);
 	/* Titre et ouverture du svg */
 	echo'<hr><h2 style="margin=1100px;text-align: center;display: block;">'.$name.'</h2><svg style="width: 1100px;height: 600px;">';
@@ -31,26 +30,13 @@ class Graph
 		for($y=0;$y<=450;$y+=$translate_y){
 			echo'<text x="40" y="'.(500-$y).'" style="text-anchor: end;">'.round($y/$translate_y, 1).'</text>';
 			echo '<path stroke-width="1px" stroke-opacity="0.3" stroke="#000" d="M 100 '.(500-$y).' l 1000 0"></path>';
-=======
-	$translate_x = 900/count($array);
-	/* Titre et ouverture du svg */
-	echo'<hr><h2 style="margin=1000px;text-align: center;display: block;">'.$name.'</h2><svg style="width: 1100px;height: 600px;">';
-		$x = 150;
-		for($y=0;$y<=450;$y+=$translate_y){
-			echo'<text x="40" y="'.(500-$y).'" style="text-anchor: end;">'.round($y/$translate_y, 1).'</text>';
-			echo '<path stroke-width="1px" stroke-opacity="0.3" stroke="#000" d="M 100 '.(500-$y).' l 900 0"></path>';
->>>>>>> develop
 		}
 
 		foreach($array as $nom => $count){
 			$y = $count*$translate_y;
 			echo '<path stroke-width="1px" stroke-opacity="0.3" stroke="#000" d="M '.($x).' 500 l 0 -500"></path>';
 			echo '<path stroke-width="2px" fill="'.rand_color().'" stroke="#000" d="M '.($x-15).' 500 l 0 -'.$y.' l 30 0 l 0 '.$y.'"></path>';
-<<<<<<< HEAD
 			$nom = wordwrap($nom, 12, "|", true);
-=======
-			$nom = wordwrap($nom, 15, "|", true);
->>>>>>> develop
 			$nom = explode('|',$nom);
 			$y = 525;
 			foreach($nom as $l){
@@ -60,10 +46,6 @@ class Graph
 			$x+= $translate_x;
 		}
 		/* Bordure et Fermeture du SVG */
-<<<<<<< HEAD
 		echo'<path stroke-width="2px" fill="transparent" stroke="#000" d="M 100 0 L 100 500 L 1100 500"></path></svg>';
-=======
-		echo'<path stroke-width="2px" fill="transparent" stroke="#000" d="M 100 0 L 100 500 L 1000 500"></path></svg>';
->>>>>>> develop
 	}
 }
